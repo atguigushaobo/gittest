e08ea3b5ad0428e7237040d223931b231685266b
layne <layne@qq.com>
2024-03-29 19:26:31 +0800

    second commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
     public static void main(String[] args){
         //学会子idea中进行添加暂存区，提交本地库操作
         System.out.println("shao");
+        System.out.println("bo");
     }
 }
